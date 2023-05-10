2a7fbfa9e6bb4e1c0360bddcdeb2744664b5cb6e
Fen9939 <66569375+Fen9939@users.noreply.github.com>
2023-05-10 23:51:00 +0100

    Updated index.py

--- index.py
+++ index.py
@@ -1,15 +1,20 @@
 import PySimpleGUI as sg
 
-layout = [ [sg.Text("Hello from PySimpleGUI")],
-          [sg.Text("What Pizza would you like?"), sg.InputText()],
-          [sg.Button("OK")] ]
+pizzaList = ["Pepperoni", "Only Cheese", "Spicy", "Vegetarian"]
+sizeList = ["Small", "Medium", "Large"]
 
-window = sg.Window("Hello World", layout)
+layout = [ [sg.Text("Your Way Pizza Parlour Order Menu")],
+          [sg.Text("What type of Pizza would you like?"), sg.Cobmo(pizzaList)],
+          [sg.Text("What size Pizza would you like?"), sg.Combo(sizeList)],
+          [sg.Button("OK"), sg.Button("Cancel")] ]
+
+window = sg.Window("Your Way Pizza Parlour", layout)
 
 while True:
     event, values = window.read()
 
-    if event == "OK" or event == sg.WIN_CLOSED:
+    if event == "Cancel" or event == sg.WIN_CLOSED:
         break
+    print("You ordered a {size} {type} pizza".format(size=values[0], type=values[1]))
 
 window.close()
