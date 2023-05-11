9c13818bf672863f2b01088429142dc4201c1ed8
Fen9939 <66569375+Fen9939@users.noreply.github.com>
2023-05-11 18:31:15 +0100

    Updated index.py

--- index.py
+++ index.py
@@ -10,48 +10,113 @@ class Pizzas:
     
     def getSize(self):
         return self.__size
+
+class Toppings:
+    def __init__(self, topping, amount):
+        self.__topping = topping
+        self.__amount = amount
+
+    def getTopping(self):
+        return self.__topping
     
-class Prices(Pizzas):
-    def __init__(self, type, size, price):
-        super().__init__(type, size)
-        self.__price = price
+    def getAmount(self):
+        return self.__amount
+    
+    def toppCost(self):
+        baseCost = 100.0
+        tempCost = 0.0
+        toppingCost = 0.0
+    
+        if(self.__topping == "Pepperoni"):
+            tempCost = baseCost*float(self.__amount)
+            toppingCost = tempCost
+        if(self.__topping == "Cheese"):
+            tempCost = (baseCost+25)*float(self.__amount)
+            toppingCost = toppingCost+tempCost
+        if(self.__topping == "Jalepenos"):
+            tempCost = (baseCost+25)*float(self.__amount)
+            toppingCost = toppingCost+tempCost
+        if(self.__topping == "Sausages"):
+            tempCost = (baseCost+40)*float(self.__amount)
+            toppingCost = toppingCost+tempCost
+        
+        return round(toppingCost/100, 2)
+
+class Sides:
+    def __init__(self, side, amount):
+        self.__side = side
+        self.__amount = amount
+
+    def getSide(self):
+        return self.__side
+    
+    def getAmount(self):
+        return self.__amount
+    
+    def sidesCost(self):
+        baseCost = 100.0
+        tempCost = 0
+        totalCost = 0
+
+class Prices(Pizzas, Toppings):
+    def __init__(self,  price):
+        super(Pizzas).__init__
+        super(Toppings).__init__
+        self.__price = price    
 
     def getPrice(self):
-        small = 0.85
-        medium = 1.0
-        large = 1.15
-        basePrice = 7.00
+        Small = 0.75
+        Medium = 1.00
+        Large = 2.00
+        XL = 2.75
+        basePrice = 800
+        finalPrice = 0
 
+        if self.getType() == "Only Cheese":
+            basePrice = basePrice+29
+        if self.getType() == "Spicy":
+            basePrice = basePrice+79
+        if self.getType() == "Meaty":
+            basePrice = basePrice+129
+        if self.getType() == "Vegetarian":
+            basePrice = basePrice+259
+        
+        if self.getSize() == "Small":
+            finalPrice = basePrice*Small
+        if self.getSize() == "Medium":
+            finalPrice = basePrice*Medium
+        if self.getSize() == "Large":
+            finalPrice = basePrice*Large
+        if self.getSize() == "Extra Large":
+            finalPrice = basePrice*XL
+            
+        finalPrice
 
-    
-pizzaOne = Prices("Pepperoni", "£3.12")
-pizzaTwo = Prices("Only Cheese", "£.29")
-pizzaThree = Prices("Spicy", "£3.29")
-pizzafour = Prices("Meaty", "£3.59")
-pizzafive = Prices("Vegetarian", "£4.59")
+        return round(finalPrice/100, 2)
 
 pizzaList = ["Pepperoni", "Only Cheese", "Spicy", "Vegetarian"]
-sizeList = ["Small", "Medium", "Large"]
+sizeList = ["Small", "Medium", "Large", "Extra Large"]
+amounts = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 
 sg.set_options(font=("Comic Sans", 16))
-sg.theme('Dark Blue 3')
+sg.theme('System Default 1')
 
 layout = [ [sg.Text("Your Way Pizza Parlour Order Menu", font=("Comic Sans", 20))],
-          [sg.Text("What type of Pizza would you like?"), sg.Combo(pizzaList, default_value="Pepperoni")],
-          [sg.Text("What size Pizza would you like?"), sg.Combo(sizeList, default_value="Medium")],
+          [sg.Text("What type of Pizza would you like?"), sg.Combo(pizzaList, default_value="Pepperoni", key="Type")],
+          [sg.Text("What size Pizza would you like?"), sg.Combo(sizeList, default_value="Medium", key="Size")],
           [sg.Text("Any Extra Toppings?")],
-          [sg.Checkbox(f"{pizzaOne.getPrice()} - {pizzaOne.getType()}", key="pep")],
-          [sg.Checkbox("Cheese", key="che")],
-          [sg.Checkbox("Jalepenos", key="jal")],
-          [sg.Checkbox("Sausage", key="sau")],
+          [sg.Spin(amounts, key="pep", readonly=True, size=3), sg.Text("Pepperoni")],
+          [sg.Spin(amounts, key="che", readonly=True, size=3), sg.Text("Cheese")],
+          [sg.Spin(amounts, key="jal", readonly=True, size=3), sg.Text("Jalepenos")],
+          [sg.Spin(amounts, key="sau", readonly=True, size=3), sg.Text("Sausages")],
           [sg.Text("Any Side Orders?")],
-          [sg.Checkbox("Garlic Herb Dip", key="pep")],
-          [sg.Checkbox("Coleslaw", key="che")],
-          [sg.Checkbox("Potato Wedges", key="jal")],
-          [sg.Checkbox("Cookies", key="sau")],
+          [sg.Spin(amounts, key="ghd", readonly=True, size=3), sg.Text("Garlic Herb Dip")],
+          [sg.Spin(amounts, key="col", readonly=True, size=3), sg.Text("Coleslaw")],
+          [sg.Spin(amounts, key="pot", readonly=True, size=3), sg.Text("Potato Wedges")],
+          [sg.Spin(amounts, key="coo", readonly=True, size=3), sg.Text("Cookies")],
           [sg.Button("OK"), sg.Button("Cancel")],
           [sg.Text("")],
-          [sg.Text("£" + Prices.getPrice())]
+          [sg.Text("£0.00", key="finalPrice")]
         ]
 
 window = sg.Window("Your Way Pizza Parlour", layout)
@@ -62,6 +127,18 @@ while True:
     if event == "Cancel" or event == sg.WIN_CLOSED:
         break
 
-    print(f"You ordered a {values[1]} {values[0]} pizza")
+    toppingsAmounts = {"Pepperoni": values["pep"], "Cheese": values["che"], "Jalepenos": values["jal"], "Sausages": values["sau"]}
+    totalPrice = 0
+
+    for i in toppingsAmounts:
+        tempPrice = Prices.toppCost(Toppings(i, toppingsAmounts[i]))
+        totalPrice = totalPrice+tempPrice
+
+    price = Pizzas(values["Type"], values["Size"])
+
+    if event == "OK":
+        window['finalPrice'].update(f"Your total is: £{Prices.getPrice(price)+totalPrice}")
+
+    print(f'You ordered a {values["Size"]} {values["Type"]} pizza\nThis will cost {Prices.getPrice(price)+totalPrice}')
 
 window.close()
