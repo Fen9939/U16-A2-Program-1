cc481057e7805fd20afc0a6f3c1b74ece4860db0
Fen9939 <66569375+Fen9939@users.noreply.github.com>
2023-05-12 12:22:50 +0100

    Finished index.py!

--- index.py
+++ index.py
@@ -1,15 +1,19 @@
 import PySimpleGUI as sg
 
 class Pizzas:
-    def __init__(self, type, size):
+    def __init__(self, type, size, amount):
         self.__type = type
         self.__size = size
+        self.__amount = amount
 
     def getType(self):
         return self.__type
     
     def getSize(self):
         return self.__size
+    
+    def getAmount(self):
+        return self.__amount
 
 class Toppings:
     def __init__(self, topping, amount):
@@ -40,7 +44,7 @@ class Toppings:
             tempCost = (baseCost+40)*self.__amount
             toppingCost = toppingCost+tempCost
         
-        return round(toppingCost/100, 2)
+        return toppingCost
 
 class Sides:
     def __init__(self, side, amount):
@@ -58,9 +62,22 @@ class Sides:
         tempCost = 0
         totalCost = 0
 
+        if(self.__side == "Garlic Herb Dip"):
+            tempCost = baseCost*self.__amount
+            totalCost = tempCost
+        if(self.__side == "Coleslaw"):
+            tempCost = (baseCost+15)*self.__amount
+            totalCost = totalCost+tempCost
+        if(self.__side == "Potato Wedges"):
+            tempCost = (baseCost+25)*self.__amount
+            totalCost = totalCost+tempCost
+        if(self.__side == "Cookies"):
+            tempCost = (baseCost+25)*self.__amount
+            totalCost = totalCost+tempCost
 
+        return totalCost
 
-class Prices(Pizzas, Toppings):
+class Prices(Pizzas, Toppings, Sides):
     def __init__(self,  price):
         super(Pizzas).__init__
         super(Toppings).__init__
@@ -91,10 +108,8 @@ class Prices(Pizzas, Toppings):
             finalPrice = basePrice*Large
         if self.getSize() == "Extra Large":
             finalPrice = basePrice*XL
-            
-        finalPrice
 
-        return round(finalPrice/100, 2)
+        return finalPrice
 
 pizzaList = ["Pepperoni", "Only Cheese", "Spicy", "Vegetarian"]
 sizeList = ["Small", "Medium", "Large", "Extra Large"]
@@ -107,6 +122,7 @@ layout = [
           [sg.Text("Your Way Pizza Parlour Order Menu", font=("Comic Sans", 20))],
           [sg.Text("What type of Pizza would you like?"), sg.Combo(pizzaList, default_value="Pepperoni", key="Type")],
           [sg.Text("What size Pizza would you like?"), sg.Combo(sizeList, default_value="Medium", key="Size")],
+          [sg.Text("How many Pizzas would you like?"), sg.Spin(amounts, initial_value=1, key="pizzaAmount")],
           [sg.Text("Any Extra Toppings?")],
           [sg.Spin(amounts, key="pep", readonly=True, size=3), sg.Text("Pepperoni")],
           [sg.Spin(amounts, key="che", readonly=True, size=3), sg.Text("Cheese")],
@@ -117,70 +133,116 @@ layout = [
           [sg.Spin(amounts, key="col", readonly=True, size=3), sg.Text("Coleslaw")],
           [sg.Spin(amounts, key="pot", readonly=True, size=3), sg.Text("Potato Wedges")],
           [sg.Spin(amounts, key="coo", readonly=True, size=3), sg.Text("Cookies")],
-          [sg.Button("OK"), sg.Button("Cancel")],
+          [sg.Button("OK"), sg.Push(), sg.Button("Close")],
         ]
 
 window = sg.Window("Your Way Pizza Parlour", layout)
 
-def orderBillWindow():
+def orderBillWindow(price, toppings, sides, pizzas):
     billCol1 = [
-        [sg.Text("Test")]
+        [sg.Text("Price for One")]
     ]
     billCol2 = [
-        [sg.Text("Test")]
+        [sg.Text("Name")],
+    ]
+    billCol3 = [
+        [sg.Text("Amount")]
     ]
 
-    billCol3 = [
-        [sg.Text("Test")]
-    ]
     billCol4 = [
-        [sg.Text("Test")]
+        [sg.Text(f"£{Prices.getPrice(pizzas)/100}")]
     ]
-    
     billCol5 = [
-        [sg.Text("Test")]
+        [sg.Text(f"{pizzas.getSize()} {pizzas.getType()}")]
     ]
     billCol6 = [
-        [sg.Text("Test")]
+        [sg.Text(pizzas.getAmount())]
     ]
 
+    billCol7 = [
+        [sg.Text("£1.00")],
+        [sg.Text("£1.25")],
+        [sg.Text("£1.25")],
+        [sg.Text("£1.40")]
+    ]
+    billCol8 = [
+        [sg.Text("Pepperoni")],
+        [sg.Text("Cheese")],
+        [sg.Text("Jalepenos")],
+        [sg.Text("Sausages")]
+    ]
+    billCol9 = [
+        [sg.Text(toppings["Pepperoni"])],
+        [sg.Text(toppings["Cheese"])],
+        [sg.Text(toppings["Jalepenos"])],
+        [sg.Text(toppings["Sausages"])]
+    ]
+
+    billCol10 = [
+        [sg.Text("£1.00")],
+        [sg.Text("£1.15")],
+        [sg.Text("£1.15")],
+        [sg.Text("£1.25")]
+    ]
+    billCol11 = [
+        [sg.Text("Garlic Herb Dip")],
+        [sg.Text("Coleslaw")],
+        [sg.Text("Potato Wedges")],
+        [sg.Text("Cookies")]
+    ]
+    billCol12 = [
+        [sg.Text(sides["Garlic Herb Dip"])],
+        [sg.Text(sides["Coleslaw"])],
+        [sg.Text(sides["Potato Wedges"])],
+        [sg.Text(sides["Cookies"])]
+    ]
+
+
     layout = [
-        [sg.Text(f"Your total is: £", justification="centre", font=("Comic Sans", 20, "bold"))],
-        [sg.Text("Pizza and Size", justification="centre", font=("Comic Sons", 16, "underline"))],
-        [sg.Column(billCol1), sg.Column(billCol2)],
-        [sg.Text("Extra Toppings", justification="centre", font=("Comic Sons", 16, "underline"))],
-        [sg.Column(billCol3), sg.Column(billCol4)],
-        [sg.Text("Side Orders", justification="centre", font=("Comic Sons", 16, "underline"))],
-        [sg.Column(billCol5), sg.Column(billCol6)],
+        [sg.Text(f"Your total is: £{price}", font=("Comic Sans", 20, "bold"))],
+        [sg.Column(billCol1), sg.Column(billCol2), sg.Column(billCol3)],
+        [sg.Text("Pizza and Size", font=("Comic Sons", 16, "underline"))],
+        [sg.Column(billCol4), sg.Column(billCol5), sg.Column(billCol6)],
+        [sg.Text("Extra Toppings", justification="center", font=("Comic Sons", 16, "underline"))],
+        [sg.Column(billCol7), sg.Column(billCol8), sg.Column(billCol9)],
+        [sg.Text("Side Orders", justification="center", font=("Comic Sons", 16, "underline"))],
+        [sg.Column(billCol10), sg.Column(billCol11), sg.Column(billCol12)],
         [sg.Button("OK")]
     ]
 
-    window = sg.Window("Order Bill", layout)
+    window = sg.Window("Order Bill", layout, element_justification="c",)
 
     while True:
         event, values = window.read()
 
         if event == "OK" or event == sg.WIN_CLOSED:
+            sg.popup("Your order has been sent to the kitchen!")
             break
+
     window.close()
 
 while True:
     event, values = window.read()
 
-    if event == "Cancel" or event == sg.WIN_CLOSED:
+    if event == "Close" or event == sg.WIN_CLOSED:
         break
 
     toppingsAmounts = {"Pepperoni": values["pep"], "Cheese": values["che"], "Jalepenos": values["jal"], "Sausages": values["sau"]}
+    sidesAmounts = {"Garlic Herb Dip": values["ghd"], "Coleslaw": values["col"], "Potato Wedges": values["pot"], "Cookies": values["coo"]}
     totalPrice = 0
 
     for i in toppingsAmounts:
         tempPrice = Prices.toppCost(Toppings(i, toppingsAmounts[i]))
         totalPrice = totalPrice+tempPrice
 
-    price = Pizzas(values["Type"], values["Size"])
-    finalPrice = Prices.getPrice(price)+totalPrice
+    for i in sidesAmounts:
+        tempPrice = Prices.sidesCost(Sides(i, sidesAmounts[i]))
+        totalPrice = totalPrice+tempPrice
+
+    price = Pizzas(values["Type"], values["Size"], values["pizzaAmount"])
+    finalPrice = (Prices.getPrice(price)+totalPrice)/100
     if event == "OK":
-        orderBillWindow()
+        orderBillWindow(finalPrice, toppingsAmounts, sidesAmounts, price)
 
     print(f'You ordered a {values["Size"]} {values["Type"]} pizza\nThis will cost {Prices.getPrice(price)+totalPrice}')
 
